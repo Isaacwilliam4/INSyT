90064e4340a0c8798ac28357730c108a139e4d35
dtingey <damon.tingey@gmail.com>
2024-01-16 16:43:19 -0700

    implement debug logging

--- insyt/__main__.py
+++ insyt/__main__.py
@@ -1,5 +1,8 @@
 import sys
+import argparse
+import logging
 from insyt.db import Database
+from insyt.file_watcher import watch_files
 
 # check must be located before importing code that uses 3.10 features
 if sys.version_info < (3, 10):
@@ -7,9 +10,35 @@ if sys.version_info < (3, 10):
 
 
 def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    #add -- arguments
+    parser.add_argument("--watch", nargs="+", help="List of files to watch")
+    parser.add_argument("--db", help="Database file to use", default="insyt.db")
+    parser.add_argument("--debug", help="Enable debug logging", action="store_true")
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
+        logging.getLogger("INSyT -- DEBUG").setLevel(logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+        logging.getLogger("INSyT").setLevel(logging.INFO)
+
+    logging.info("Starting INSyT")
+    logging.debug(f"Command line arguments: {args}")
+
+    file_list = args.watch
+    logging.debug(f"Configuring the following files to watch: {file_list}")
+
     # Create database object
-    db = Database("insyt.db")
-    
+    db = Database(args.db)
+    logging.debug(f"Using database file: {args.db}")
+
+    logging.debug("Starting file watcher")
+    # Watch files
+    watch_files(file_list)
+
     #this is meant just to test some functionality during development TODO: Add actual runtime functionality at the end
     breakpoint()
 
--- insyt/file_watcher.py
+++ insyt/file_watcher.py
@@ -1,9 +1,8 @@
 import time
+import logging
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
-from inference import run_model
 
 class FileWatcherHandler(FileSystemEventHandler):
     def __init__(self, file_list):
@@ -11,7 +10,9 @@ class FileWatcherHandler(FileSystemEventHandler):
         self.file_history = {file: [] for file in file_list}
 
     def on_modified(self, event):
+        logging.debug(f'File {event.src_path} was modified')
         if event.src_path in self.file_list:
+            logging.debug(f'File {event.src_path} is in the list of files to watch')
             with open(event.src_path, 'r') as f:
                 lines = f.readlines()
                 if len(lines) > len(self.file_history[event.src_path]):
@@ -23,6 +24,7 @@ class FileWatcherHandler(FileSystemEventHandler):
 
 def watch_files(file_list):
     event_handler = FileWatcherHandler(file_list)
+    logging.info(f'Starting observers on files: {file_list}')
     observer = Observer()
     for file in file_list:
         observer.schedule(event_handler, path=file, recursive=False)
@@ -34,6 +36,5 @@ def watch_files(file_list):
         observer.stop()
     observer.join()
 
-
-def add_to_db(file_path, line_number, line, previous_lines):
-    pass
+def add_to_db(database, file_path, line_number, line, previous_lines):
+    logging.debug(f'Adding to DB: {file_path}, {line_number}, {line}, {previous_lines}')
