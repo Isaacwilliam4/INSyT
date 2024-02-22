0863f218a9c89853ee94254f2e44595733efe44f
dtingey <damon.tingey@gmail.com>
2024-02-21 17:11:09 -0700

    nothing

---
+++ insyt/daemon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while true
+do
+    python -m insyt --run /path/to/dir
+done
--- insyt/inference.py
+++ insyt/inference.py
@@ -3,4 +3,4 @@ import random
 # TODO: replace with actual model (Isaac/Bronze) Maybe move to a different directory if necessary
 def run_model(file_line, previous_lines):
     # randomly return a number 0-6, weighted 50% towards 0 and 10% towards 1-6
-    return random.choices(range(7), weights=[6, 1, 1, 1, 1, 1, 1])[0]
+    return random.choices(range(7), weights=[10, 1, 1, 1, 1, 1, 1])[0]
