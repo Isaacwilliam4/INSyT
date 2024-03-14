216f21800fe52d1743273cb859495d91235b250a
dtingey <damon.tingey@gmail.com>
2024-03-14 17:53:47 -0600

    more cleanup

--- insyt/ollama.py
+++ insyt/ollama.py
@@ -6,24 +6,6 @@ from insyt.db import Database
 log = logging.getLogger("INSyT")
 
 
-# def run_model(file_line, previous_lines, classification):
-#     """Run the analysis model given the line and context"""
-#     log.debug(f"Running analysis model on line: {file_line}")
-#     log.debug(f"Previous lines: {previous_lines}")
-#     log.debug(f"Classification: {classification}")
-
-#     prompt = (
-#         f"The following line has been classified as '{classification}':\n\n```{file_line}```\n\n"
-#         + "Explain why it was classified as such and how I should respond."
-#     )
-
-#     response = ollama.chat(
-#         model="insyt", messages=[{"role": "user", "content": prompt}]
-#     )
-#     log.debug(f"Response: {response}")
-#     return response["message"]["content"]
-
-
 def run_model(id, file_line, previous_lines, classification):
     """Run the analysis model given the line and context"""
     log.debug(f"Running analysis model on line: {file_line}")
